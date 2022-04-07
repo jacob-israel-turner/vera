8c02488810e82e63ab38e0e9c1cbcd032515c527
Jacob Turner <j.israel.turner@gmail.com>
2022-04-06 22:52:36 -0600

    Add api/

---
+++ packages/api/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/api',
+};
